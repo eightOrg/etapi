0acaf38d5019be0c8521ff9b68c5a696043b57ea
miaoch <476864279@qq.com>
2016-10-21 10:40:47 +0800

    area 未测试

---
+++ src/main/java/com/eight/dao/AreaDao.java
@@ -0,0 +1,12 @@
+package com.eight.dao;
+
+import com.eight.pojo.Area;
+import com.eight.trundle.db.dao.BaseDao;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Administrator on 2016/10/21.
+ */
+@Component
+public interface AreaDao extends BaseDao<Area> {
+}
